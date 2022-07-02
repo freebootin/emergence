1422abef6b15c1298583d59dfdfdc368911a74b8
freebootin <patrick@freebootin.com>
2022-07-01 19:22:15 -0700

    can draw rectangles

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
 const height = 600;
-const width = 400;
+const width = 600;
+const canvas = document.querySelector('#canvas');
+const c = canvas.getContext('2d');
 
-const canvas = querySelector('#canvas');
 canvas.height = height;
 canvas.width = width;
+
+
+function animate() {
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, canvas.width, canvas.height);
+
+  // update game state and draw screen
+  c.beginPath();
+  c.strokeStyle = 'green';
+  c.rect(20, 20, 100, 100);
+  c.stroke();
+
+}
+
+// Call animate last
+animate();
