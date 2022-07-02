a50020e47412532eba036e208822b5aa5c7264bc
freebootin <patrick@freebootin.com>
2022-07-01 19:58:39 -0700

    added drawBoard function

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const width = 600;
 const canvas = document.querySelector('#canvas');
 const c = canvas.getContext('2d');
 
+// gameboard
+const gameboard = [ 'blue', 'green', 'yellow', 'red' ];
+
 canvas.height = height;
 canvas.width = width;
 
@@ -13,11 +16,21 @@ function drawSquare(xStart, yStart, width, height, color) {
   c.stroke();
 }
 
+function drawBoard(board) {
+  let xStart = 50;
+  let yStart = 50;
+
+  for (let i = 0; i < board.length; i++) {
+    let nextX = xStart + (xStart * i);
+    drawSquare(nextX, yStart, 50, 50, board[i]);
+  }
+}
+
 function animate() {
   requestAnimationFrame(animate);
   c.clearRect(0, 0, canvas.width, canvas.height);
 
-  drawSquare(100, 100, 20, 100, 'pink');
+  drawBoard(gameboard);
 
   // update game state and draw screen
 
