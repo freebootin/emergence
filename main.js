9b737214dbb11bd0c219fa9951c49af3aca93399
freebootin <patrick@freebootin.com>
2022-07-01 19:52:04 -0700

    added drawSquare function

--- main.js
+++ main.js
@@ -6,16 +6,20 @@ const c = canvas.getContext('2d');
 canvas.height = height;
 canvas.width = width;
 
+function drawSquare(xStart, yStart, width, height, color) {
+  c.beginPath();
+  c.strokeStyle = color;
+  c.rect(xStart, yStart, width, height);
+  c.stroke();
+}
 
 function animate() {
   requestAnimationFrame(animate);
   c.clearRect(0, 0, canvas.width, canvas.height);
 
+  drawSquare(100, 100, 20, 100, 'pink');
+
   // update game state and draw screen
-  c.beginPath();
-  c.strokeStyle = 'green';
-  c.rect(20, 20, 100, 100);
-  c.stroke();
 
 }
 
